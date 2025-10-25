9b5c81e044058b3d4410d8d0a4954710ba3bf7a3
donedonedone <164561078+donedonedone278@users.noreply.github.com>
2025-10-25 15:07:46 -0500

    Modify netcode-patch for v73
    Using suggested parameters directly from the modding discord. Old users may need to uninstall/reinstall netcode-patch

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -9,7 +9,7 @@ $name = $csproj.Project.PropertyGroup.AssemblyName
 if ($Release) {
 	dotnet build -c Release
 	if (!$?) { exit 1 }
-	netcode-patch ".\bin\Release\netstandard2.1\$name.dll" "..\..\Lethal Company_Data\Managed"
+	netcode-patch -uv 2022.3.62 -nv 1.12.0 -tv 1.0.0 ".\bin\Release\netstandard2.1\$name.dll" "..\..\Lethal Company_Data\Managed"
 	Copy-Item ".\bin\Release\netstandard2.1\$name.dll" "..\..\BepInEx\plugins\"
 	Copy-Item ".\bin\Release\netstandard2.1\$name.pdb" "..\..\BepInEx\plugins\"
 	Remove-Item "..\..\BepInEx\scripts\$name*.dll" -ErrorAction SilentlyContinue
@@ -20,11 +20,11 @@ if ($Release) {
 	$null = mkdir -Force "..\..\BepInEx\scripts"
 	Copy-Item ".\bin\Debug\netstandard2.1\$name.dll" "..\..\BepInEx\scripts\"
 	Copy-Item ".\bin\Debug\netstandard2.1\$name.pdb" "..\..\BepInEx\scripts\"
-	netcode-patch ".\bin\Debug\netstandard2.1\$name.dll" "..\..\Lethal Company_Data\Managed"
+	netcode-patch -uv 2022.3.62 -nv 1.12.0 -tv 1.0.0 ".\bin\Debug\netstandard2.1\$name.dll" "..\..\Lethal Company_Data\Managed"
 	Remove-Item "..\..\BepInEx\plugins\$name.dll" -ErrorAction SilentlyContinue
 	Remove-Item "..\..\BepInEx\plugins\$name.pdb" -ErrorAction SilentlyContinue
-	Copy-Item ".\bin\Debug\netstandard2.1\$name.dll" "..\..\BepInEx\scripts\"
-	Copy-Item ".\bin\Debug\netstandard2.1\$name.pdb" "..\..\BepInEx\scripts\"
+	# Copy-Item ".\bin\Debug\netstandard2.1\$name.dll" "..\..\BepInEx\scripts\" I think debug is broken
+	# Copy-Item ".\bin\Debug\netstandard2.1\$name.pdb" "..\..\BepInEx\scripts\"
 }
 
 $cfg = "..\..\BepInEx\config\BepInEx.cfg"
